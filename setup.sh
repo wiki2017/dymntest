fc88ee2448561a40aa2e67f588ab3dbf30fbfc2a
j05eph2000 <47005688+j05eph2000@users.noreply.github.com>
2019-11-18 00:15:00 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -52,7 +52,7 @@ fi
   wget --load-cookies cookies.txt -O $filename \
      'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
 
-  export fileid=1Kq75J3VDXYd-2D3Ve6i9hCuejD_KNhRt
+  export fileid=1GiSVHogUMeePxPbjuyDwg6jgYLrN7jbm
   export filename=bootstrap.zip
   wget --save-cookies cookies.txt 'https://docs.google.com/uc?export=download&id='$fileid -O- \
      | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
